4d73ddcc1b6429d00bbfa6957d81a1ed9e7f63d0
Harsh Vartak <50980498+Harshvartak@users.noreply.github.com>
2019-08-11 01:24:35 +0530

    A bit of correction to final addition

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# wordcount
+wordcount
--- wordcount/views.py
+++ wordcount/views.py
@@ -1,8 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
+import re
 
 def home(request):
 	return render(request,'home.html')
 
-def eggs(request):
-	return HttpResponse('Eggs are great')	
+def count(request):
+	fulltext=request.GET['fulltext']
+	wordlist=fulltext.split()		
+
+	wordcounter={}
+
+	wordlist2=[re.sub(r'[^A-Za-z0-9]+', '', word) for word in wordlist]
+
+
+	for word in wordlist2:
+		if word in wordcounter:
+			#Increase the counter for word
+			wordcounter[word]+=1
+		else:
+			wordcounter[word]=1
+
+	wordcounter2=sorted(wordcounter.items(), key=operator.itemgetter(1), reverse=True)		
+			
+
+	return render(request,'count.html',{'fulltext':fulltext,'count':len(wordlist2),'wordcounter':wordcounter2})
+
+def about(request):
+	return render(request,'about.html')		
